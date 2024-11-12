d269a2520d3f3e1875f668e786ae1116f346007c
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2024-11-12 13:33:59 +0900

    chore: image, svg, style setting

---
+++ public/svgs/index.ts
@@ -0,0 +1,17 @@
+import IconHamburger from './hamburger.svg';
+import IconInstagram from './instagram.svg';
+import IconKakao from './kakao.svg';
+import IconPlus from './plus.svg';
+import IconXRounded from './x-rounded.svg';
+import AutoRepoNavLogo from './autoRepo-nav-logo.svg';
+import HomeText from './home-text.svg';
+
+export {
+    IconHamburger,
+    IconInstagram,
+    IconKakao,
+    IconPlus,
+    IconXRounded,
+    AutoRepoNavLogo,
+    HomeText,
+};
