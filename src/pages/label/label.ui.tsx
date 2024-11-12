a04eb2ac0db2f09588e272cad0e43a3377c6b699
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2024-11-12 13:38:40 +0900

    feat : Setting FSD UI

---
+++ src/pages/label/label.ui.tsx
@@ -0,0 +1,13 @@
+import { ReactElement } from 'react';
+
+const LabelPage = (): ReactElement => {
+    return (
+        <div className="mx-auto h-lvh w-4/5 border-4 border-solid border-primary-500 mt-20">
+            <div className="text-h5 text-neutral-700 ">
+                <p>text</p>
+            </div>
+        </div>
+    );
+};
+
+export default LabelPage;
---
+++ src/pages/template-pr/template-pr.ui.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from 'react';
+
+const TemplatePrPage = (): ReactElement => {
+    return (
+        <div className="mx-auto h-lvh w-4/5 border-4 border-solid border-primary-500">
+            <h1 className="text-h1 text-primary-500">AutoRep TemplatePrPage</h1>
+        </div>
+    );
+};
+
+export default TemplatePrPage;
